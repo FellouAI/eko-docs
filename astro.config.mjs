363f6309c2d67801b690d24d3a02f20008e22b36
codevoyager1984 <codevoyager.jay@gmail.com>
2025-07-08 10:56:17 +0800

    chore: update Astro configuration with site URL and update yarn.lock for dependency management

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://fellou.ai',
 	base: '/eko/docs',
 	output: 'static',
 	outDir: './dist',
